97b5ff5f3fc8accb65c8797c71a08d4de4c95d48
RobertH1993 <r.hendriks1993@gmail.com>
2023-08-15 10:48:55 +0200

    Added some magic methods to the config class to enable functions like in, del and so on

--- src/config.py
+++ src/config.py
@@ -16,9 +16,15 @@ class Config:
     def __setitem__(self, key, value):
         self.config_data[key] = value
 
+    def __contains__(self, item):
+        return item in self.config_data
+
+    def __delitem__(self, key):
+        del self.config_data[key]
+
     def save(self):
         with open(self.config_path, 'w') as f:
-            json.dump(self.config_data, f)
+            json.dump(self.config_data, f, indent=1)
 
     def reload(self):
         with open(self.config_path, 'r') as f:
--- src/test/test_config.py
+++ src/test/test_config.py
@@ -13,3 +13,23 @@ class TestConfig(unittest.TestCase):
         # Reload config to pull data from disk, we want to test if it gets actually saved to disk
         test_config.reload()
         self.assertEqual(test_config['write_test_key'], test_token)
+
+    def test_contains_method(self):
+        test_config = Config("test/data/config.json")
+        self.assertNotIn('test_contains', test_config)
+        test_config['test_contains'] = True
+        self.assertIn('test_contains', test_config)
+
+    def test_del_method(self):
+        test_config = Config("test/data/config.json")
+        test_config['test_del'] = True
+        del test_config['test_del']
+        self.assertNotIn('test_del', test_config)
+
+    def test_change_and_reload_config(self):
+        test_config = Config("test/data/config.json")
+        test_config['test_reload'] = secrets.token_hex(16)
+
+        test_config.reload()
+        self.assertNotIn('test_reload', test_config)
+
